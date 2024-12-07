4d2a00a73655e5d73f52fae2949c7ce786e0346a
Guy Blass <83311385+Guyblass@users.noreply.github.com>
2024-12-07 23:39:33 +0200

    Update set_env_variables.sh

--- modules/q_and_a_dataset_generator/set_env_variables.sh
+++ modules/q_and_a_dataset_generator/set_env_variables.sh
@@ -1,10 +1,10 @@
 # open a Paper account at https://alpaca.markets/ and get these 2 values
-export ALPACA_API_KEY="YOUR_ALPACA_API_KEY"
-export ALPACA_API_SECRET="YOUR_ALPACA_API_SECRET"
+export ALPACA_API_KEY="CK2UH4N65HVLOP6DMU8T"
+export ALPACA_API_SECRET="cluJld7oo6Wca0RZq3yhJlMdzugfEssTR08cNTBd"
 
 # create a free tier account at https://cloud.qdrant.io and get this value
-export QDRANT_API_KEY="YOUR_QDRANT_API_KEY"
-export QDRANT_API_URL="YOUR_QDRANT_CLUSTER_URL"
+export QDRANT_API_KEY="CGjVqhgfs2MF7WtmXr2WPeL6V3wacp9-f5sVoXhP8dFAYor91sWxeg"
+export QDRANT_API_URL="https://221dfb59-6391-4a8a-ac31-962ad3cc541a.us-east-1-0.aws.cloud.qdrant.io"
 
 # create a free account at https://beta.openai.com/ and get this value
-export OPENAI_API_KEY="YOUR_OPEN_AI_API_KEY"
+export OPENAI_API_KEY="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"
